e82ebb0df28b986f70199fefa45fff5b330a3025
Ubuntu <ubuntu@ip-172-31-44-187.ec2.internal>
2020-02-02 15:30:13 +0000

    pip freeze requirements.txt file to address build failure in Heroku

--- env.py
+++
@@ -1,6 +0,0 @@
-# import os
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:johnbell@myfirstcluster-20lqh.mongodb.net/walking_app?retryWrites=true&w=majority')
