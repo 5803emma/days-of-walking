f418f5b099b9b057c89d13f8c6fa4ef615f305d1
Ubuntu <ubuntu@ip-172-31-44-187.ec2.internal>
2020-02-02 04:06:05 +0000

    Connected to database

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectID
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -11,25 +11,11 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:johnbell@myfirstcluster-20lqh.mong
 mongo = PyMongo(app)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/get_walk_names")
+def get_walk_names():
+    return render_template("walks.html", walks=mongo.db.walks.find())
     
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
-@app.route("/add_walk")
-def add_walk ():
-    return render_template("add_walk.html")
-    
-@app.route("/leave_review")
-def leave_review ():
-    return render_template("leave_review.html")
-    
-@app.route("/read_review")
-def read_review():
-    return render_template("read_review.html")
-    
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
