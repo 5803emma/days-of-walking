8e9af6275879c4a21dc4392311735932ba35fdd6
Ubuntu <ubuntu@ip-172-31-44-187.ec2.internal>
2020-02-02 16:23:49 +0000

    Added env.py and gitignore

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'walking_app'
-app.config["MONGO_URI"] = 'mongodb+srv://root:johnbell@myfirstcluster-20lqh.mongodb.net/walking_app?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
---
+++ env.py
@@ -0,0 +1,2 @@
+import os
+os.environ["MONGO_URI"] = "mongodb+srv://root:johnbell@myfirstcluster-20lqh.mongodb.net/walking_app?retryWrites=true&w=majority"
