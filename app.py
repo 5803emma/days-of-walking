121acf9b6389aee6fa11add0b0c5ac28608a8e3f
Ubuntu <ubuntu@ip-172-31-44-187.ec2.internal>
2020-02-02 01:17:51 +0000

    Set up to make connection to MongoDB database

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectID
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'walking_app'
+app.config["MONGO_URI"] = 'mongodb+srv://root:johnbell@myfirstcluster-20lqh.mongodb.net/walking_app?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
